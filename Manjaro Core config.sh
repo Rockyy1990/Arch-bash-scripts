fd6316aa15cc0a2cbcbe1d0490b3e508ac14d238
Rockyy1990 <56369930+Rockyy1990@users.noreply.github.com>
2022-08-07 17:00:18 +0200

    Add files via upload

--- Manjaro Core config.sh
+++ Manjaro Core config.sh
@@ -1,14 +1,23 @@
-#! /bin/bash
+#!/usr/bin/env bash
 
 echo "----------------------------------------------"
 echo "..Manjaro-Minimal XFCE4 config after install.."
-echo " ..ext4 und seperate Home Partition..         "
+echo "   with Dracut, Timeshift & Paru Aur Helper   "
 echo "----------------------------------------------"
 sleep 4
+
 sudo pacman-mirrors --country Germany
 sudo pacman -Sy
-sudo pacman -Rs midori parole --noconfirm
-sudo pacman -S timeshift timeshift-autosnap 
+sudo pacman -Rs midori parole gparted pamac-gtk libpamac-flatpak-plugin manjaro-application-utility manjaro-hello --noconfirm
+sudo pacman -S dkms timeshift timeshift-autosnap 
+clear
+
+echo "Install Paru AUR Helper"
+sudo pacman -S --needed base-devel
+git clone https://aur.archlinux.org/paru.git
+cd paru
+makepkg -si
+cd
 clear
 
 echo "Pipewire statt pulseaudio"
@@ -17,37 +26,52 @@ sudo pacman -Rs pulseaudio-zeroconf manjaro-pulse
 sudo pacman -S pipewire pipewire-alsa pipewire-pulse gst-plugin-pipewire wireplumber
 clear
 
+echo "Dracut for building initramfs images"
+sleep 2
+paru -S dracut dracut-hook-uefi rebuild-initramfs-dracut rebuild-initramfs-dracut-hook
+clear
+
 echo "System upgrade"
 sleep 3 
 sudo pacman -Syyu
 clear
+
 echo "Fonts"
 sudo pacman -S ttf-dejavu ttf-droid  ttf-liberation ttf-ubuntu-font-family
 clear
+
 echo "Firefox"
 sudo pacman -S firefox firefox-i18n-de firefox-adblock-plus
 clear
+
 echo "Multimedia"
 sudo pacman -S --needed vlc strawberry lame flac libsoxr manjaro-gstreamer gstreamer-vaapi gst-plugins-espeak
 clear
 
 echo "Nützliche Programme & System tools"
-sudo pacman -S fakeroot base-devel jre-openjdk-headless neofetch yay gsmartcontrol gnome-text-editor ocl-icd lib32-libldap vkd3d lib32-vkd3d vulkan-tools ntp gufw ufw-extras nss gnome-disk-utility file-roller libreoffice-fresh-de libreoffice-fresh shotwell udftools xfsdump f2fs-tools mtools transmission-gtk libva mono yt-dlp
+sleep 2
+sudo pacman -S --needed fakeroot base-devel jre-openjdk-headless neofetch gsmartcontrol gnome-text-editor ocl-icd lib32-libldap vkd3d lib32-vkd3d vulkan-tools ntp gufw ufw-extras nss gnome-disk-utility file-roller libreoffice-fresh-de libreoffice-fresh shotwell udftools xfsdump f2fs-tools jfsutils mtools aria2 cabextract wimlib chntpw cdrtools transmission-gtk libva mono yt-dlp
 
 sudo systemctl enable ufw.service 
 echo "neofetch" >> ~/.bashrc
 clear
 
 echo "ZSH"
-sudo pacman -S zsh zsh-autosuggestions zsh-syntax-highlighting zsh-completions
+sleep 2
+sudo paru -S zsh zsh-autosuggestions zsh-syntax-highlighting zsh-completions zsh-fish
 chsh -s /usr/bin/zsh
 
 clear
 echo "Installation von AUR Paketen"
 sleep 3
-yay -S grub-hook nohang-git auto-cpufreq ttf-ms-fonts mcmojave-circle-icon-theme elementary-xfce-icons-git systemd-zram 
+paru -S grub-hook nohang-git auto-cpufreq ttf-ms-fonts mcmojave-circle-icon-theme  systemd-zram 
 sudo systemctl condrestart systemd-zram  
+clear
 
+echo "Linux Mint Icons & Themes"
+sleep 2
+paru -S mint-themes mint-x-icons mint-backgrounds-vanessa
+clear
 
 echo "Flatpak"
 sleep 3
@@ -66,6 +90,7 @@ sudo systemctl enable --now cups.path
 clear
 
 echo "Netzwerk Drucker Support"
+sleep 2
 sudo pacman -S avahi
 sudo systemctl enable --now avahi-daemon.service
 
@@ -78,17 +103,17 @@ clear
 
 echo "Steam Gaming Plattform installation"
 sleep 3
-yay -S --needed steam protontricks steam-tweaks libxcomposite lib32-libxcomposite libxinerama lib32-libgcrypt libgcrypt lib32-libxinerama ncurses lib32-ncurses opencl-icd-loader lib32-opencl-icd-loader libxslt lib32-libxslt libva lib32-libva gtk3 lib32-gtk3 gst-plugins-base-libs lib32-gst-plugins-base-libs 
+paru -S --needed steam protontricks steam-tweaks libxcomposite lib32-libxcomposite libxinerama lib32-libgcrypt libgcrypt lib32-libxinerama ncurses lib32-ncurses opencl-icd-loader lib32-opencl-icd-loader libxslt lib32-libxslt libva lib32-libva gtk3 lib32-gtk3 gst-plugins-base-libs lib32-gst-plugins-base-libs 
 	
 echo "hrtf = true" >> ~/.alsoftrc
 
 echo "IPTV"
 sleep 2
-yay -S hypnotix
+paru -S hypnotix
 
 echo "Bluray wiedergabe"
 sleep 2
-yay -S aacskeys makemkv-libaacs libbluray
+paru -S aacskeys makemkv-libaacs libbluray
 
 echo "SSD optimieren"
 sleep 2
@@ -101,13 +126,13 @@ echo ""
 echo "System Bereinigung.."
 echo 
 sleep 3
-yay -c
+paru -c
 sudo paccache -r
 sudo fstrim -av
 clear
 history -c
 echo ""
 
-echo "Sytem erfolgreich eingerichtet.. Neustart in 10 sekunden"
+echo "..Sytem erfolgreich eingerichtet.. Neustart in 10 sekunden!"
 sleep 10
 reboot
