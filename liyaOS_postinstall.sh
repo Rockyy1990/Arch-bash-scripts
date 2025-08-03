24c9d47bbd48e9b7b83a060b911d57e3a68c268d
Rockyy1990 <56369930+Rockyy1990@users.noreply.github.com>
2025-08-03 10:33:28 +0200

    Add files via upload

--- liyaOS_postinstall.sh
+++ liyaOS_postinstall.sh
@@ -3,6 +3,7 @@
 echo ""
 echo "LiyaOS Postinstall"
 read -p "Press any key to continue..."
+echo ""
 
 sudo pacman -R noconfirm brave-bin gnome-chess-classic gnuchess gnome-robots perl-www-robotrules timeshift timeshift-autosnap geary pinta bleachbit
 sudo pacman -R proton-vpn-gtk-app python-proton-core python-proton-vpn-network-manager python-proton-vpn-local-agent
@@ -12,7 +13,7 @@ sudo pacman-key --populate
 sudo pacman -Syu
 clear
 
-sudo pacman -S --needed --noconfirm lib32-mesa lib32-mesa-utils opencl-mesa vulkan-radeon vulkan-swrast vulkan-mesa-layers vulkan-dzn vulkan-exta-layers
+sudo pacman -S --needed --noconfirm lib32-mesa lib32-mesa-utils opencl-mesa vulkan-radeon vulkan-swrast vulkan-mesa-layers vulkan-dzn vulkan-extra-layers
 
 sudo pacman -S --needed --noconfirm yay xfsdump  vivaldi vivaldi-ffmpeg-codecs thunderbird
 sudo pacman -S --needed soundconverter yt-dlp handbrake gstreamer-vaapi pipewire-v4l2 pipewire-zeroconf gst-plugin-pipewire 
