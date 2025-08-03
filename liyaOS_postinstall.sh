d51318d0f51ecf86bf4a86fcc3ea269b01f7133f
Rockyy1990 <56369930+Rockyy1990@users.noreply.github.com>
2025-08-03 12:20:24 +0200

    Add files via upload

--- liyaOS_postinstall.sh
+++ liyaOS_postinstall.sh
@@ -4,8 +4,9 @@ echo ""
 echo "LiyaOS Postinstall"
 read -p "Press any key to continue..."
 echo ""
+clear
 
-sudo pacman -R --noconfirm brave-bin gnome-chess-classic gnuchess gnome-robots perl-www-robotrules timeshift timeshift-autosnap geary pinta bleachbit
+sudo pacman -R --noconfirm brave-bin gnome-chess-classic gnuchess gnome-robots perl-www-robotrules timeshift timeshift-autosnap geary pinta bleachbit celluloid
 sudo pacman -R proton-vpn-gtk-app python-proton-core python-proton-vpn-network-manager python-proton-vpn-local-agent
 
 sudo pacman-key --init
@@ -13,13 +14,19 @@ sudo pacman-key --populate
 sudo pacman -Syu
 clear
 
-sudo pacman -S --needed --noconfirm lib32-mesa lib32-mesa-utils opencl-mesa vulkan-radeon vulkan-swrast vulkan-mesa-layers vulkan-dzn vulkan-extra-layers
+sudo pacman -S --needed --noconfirm lib32-mesa lib32-mesa-utils opencl-mesa vulkan-radeon vulkan-swrast
+sudo pacman -S --needed --noconfirm vulkan-mesa-layers vulkan-dzn vulkan-extra-layers vulkan-validation-layers volk
+sudo pacman -S --needed --noconfirm mint-l-icons mint-x-icons mint-l-theme mintstick
+sudo pacman -S --needed nomachine 
 
-sudo pacman -S --needed --noconfirm yay xfsdump  vivaldi vivaldi-ffmpeg-codecs thunderbird
-sudo pacman -S --needed soundconverter yt-dlp handbrake gstreamer-vaapi pipewire-v4l2 pipewire-zeroconf gst-plugin-pipewire 
+sudo pacman -S --needed --noconfirm yay xfsdump  vivaldi vivaldi-ffmpeg-codecs thunderbird pavucontrol
+sudo pacman -S --needed soundconverter yt-dlp handbrake smplayer gstreamer-vaapi pipewire-v4l2 pipewire-zeroconf gst-plugin-pipewire 
 
-sudo pacman -S --needed --noconfirm steam bottles protonup-qt protontricks winetricks wine wine-mono libgdiplus openal
+sudo pacman -S --needed --noconfirm steam steam-native-runtime python-steam bottles protonup-qt protontricks winetricks wine wine-mono libgdiplus 
 yay -S faudio
+
+flatpak install flatseal
+
 clear
 
 sudo pacman -Scc --noconfirm
@@ -28,6 +35,7 @@ sudo systemctl enable fstrim.timer
 sudo fstrim -av 
 
 clear
+echo ""
 echo "Postinstall is now complete."
 read -p "Press any key to reboot.."
 sudo reboot
