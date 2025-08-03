313036c3e0c4c072459de751d7f8a6696561a45b
Rockyy1990 <56369930+Rockyy1990@users.noreply.github.com>
2025-08-03 10:30:28 +0200

    Add files via upload

---
+++ liyaOS_postinstall.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+echo ""
+echo "LiyaOS Postinstall"
+read -p "Press any key to continue..."
+
+sudo pacman -R noconfirm brave-bin gnome-chess-classic gnuchess gnome-robots perl-www-robotrules timeshift timeshift-autosnap geary pinta bleachbit
+sudo pacman -R proton-vpn-gtk-app python-proton-core python-proton-vpn-network-manager python-proton-vpn-local-agent
+
+sudo pacman-key --init
+sudo pacman-key --populate
+sudo pacman -Syu
+clear
+
+sudo pacman -S --needed --noconfirm lib32-mesa lib32-mesa-utils opencl-mesa vulkan-radeon vulkan-swrast vulkan-mesa-layers vulkan-dzn vulkan-exta-layers
+
+sudo pacman -S --needed --noconfirm yay xfsdump  vivaldi vivaldi-ffmpeg-codecs thunderbird
+sudo pacman -S --needed soundconverter yt-dlp handbrake gstreamer-vaapi pipewire-v4l2 pipewire-zeroconf gst-plugin-pipewire 
+
+sudo pacman -S --needed --noconfirm steam bottles protonup-qt protontricks winetricks wine wine-mono libgdiplus openal
+yay -S faudio
+clear
+
+sudo pacman -Scc --noconfirm
+
+sudo systemctl enable fstrim.timer
+sudo fstrim -av 
+
+clear
+echo "Postinstall is now complete."
+read -p "Press any key to reboot.."
+sudo reboot
+
