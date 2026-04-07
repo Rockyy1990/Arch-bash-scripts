029ce6f2c67d680892360ee9485f82eeddd868a3
Rockyy1990 <56369930+Rockyy1990@users.noreply.github.com>
2026-04-08 00:18:45 +0200

    Add files via upload

--- firmware-updates.sh
+++ firmware-updates.sh
@@ -1,10 +1,27 @@
 #!/bin/bash
 
+echo ""
 sudo pacman -S --needed --noconfirm fwupd fwupd-efi
+clear
 
+echo ""
 read -p "Firmware updates. Press any key to continue.."
+clear
 
+echo ""
 sudo fwupdmgr get-devices
 sudo fwupdmgr refresh --force
+sleep 3
+
+echo ""
+echo "Hole Firmware updates..."
 sudo fwupdmgr get-updates -y
+
+echo ""
+echo "Führe firmware updates aus..."
 sudo fwupdmgr update -y
+
+echo ""
+echo "Fertig. Das System wird nun neu gestartet.."
+sleep 4
+sudo reboot
